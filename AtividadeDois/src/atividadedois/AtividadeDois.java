164e450c1dd5a21a4a578a871b610845ca0ccb66
Mussatos <antonio.muszato@gmail.com>
2023-11-01 21:34:11 -0400

    Commit1

---
+++ AtividadeDois/src/atividadedois/AtividadeDois.java
@@ -0,0 +1,17 @@
+
+package atividadedois;
+
+public class AtividadeDois {
+
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World!");
+        
+        
+        
+        
+        
+    }
+    
+}
